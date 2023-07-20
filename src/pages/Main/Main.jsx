894e5f3518efded259fe99cb83931bf60ffe297a
Bohdan <bpapushoj@gmail.com>
2023-07-20 12:52:13 +0300

    create page Main.js

---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,14 @@
+import { Section } from "./Main.styled";
+
+export default function Main() {
+  return (
+    <Section>
+      <h1>Choose project</h1>
+      <ul>
+        <li>Find Image</li>
+        <li>Movies</li>
+        <li>Phone book</li>
+      </ul>
+    </Section>
+  );
+}
---
+++ src/pages/Main/Main.styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  padding-top: 30px;
+  padding-bottom: 30px;
+  background-color: #070606;
+  color: #fff;
+`;
