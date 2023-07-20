17f5a5817f31dece25c1f18d77dcb7c2537e5274
Bohdan <bpapushoj@gmail.com>
2023-07-20 18:42:05 +0300

    1

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,10 +1,16 @@
-export const Error = () => { 
-    return (
-        <div style={{
-            textAlign: 'center',
-        }}>
-            <h1>404</h1>
-            <p>Page not found</p>
-        </div>
-    )
+import { useLocation } from "react-router-dom";
+
+export const Error = () => {
+  const location = useLocation();
+  console.log(location);
+  return (
+    <div
+      style={{
+        textAlign: "center",
+      }}
+    >
+      <h1>404</h1>
+      <p>Page not found</p>
+    </div>
+  );
 };
